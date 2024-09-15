41e82722740a7c0856f7fb21a023f62fa86e2c5d
Rimon pramanik <remonkumar21@gmail.com>
2024-09-15 13:29:18 +0600

    last update document

--- script/Alpha-clash.js
+++ script/Alpha-clash.js
@@ -3,6 +3,7 @@
 //     //     step-1: hide the home screen . to hide the screen add the class didden to the home scren
 //     const homesection = document.getElementById('home-scren');
 //     homesection.classList.add('hidden')
+// jibon tai bedona
 
 //     // console.log(homesection.classList)
 //     const playGround = document.getElementById('play-ground')
@@ -10,60 +11,60 @@
 // }
 
 
- function handleKeyButtonPress (event){
+function handleKeyButtonPress(event) {
     const playerPressed = event.key;
-    console.log('player press',playerPressed);
+    console.log('player press', playerPressed);
 
-    if(playerPressed === 'Escape'){
+    if (playerPressed === 'Escape') {
         gameOver();
     }
 
 
     // get the expeted to the press
     const currentAlphabateElement = document.getElementById('current-alphabate');
-    const currentAlphabate= currentAlphabateElement.innerText;
+    const currentAlphabate = currentAlphabateElement.innerText;
     const expetedAlphabate = currentAlphabate.toLowerCase()
     // console.log(playerPressed, expetedAlphabate);
 
 
     // check mach the alphabate
-    if(playerPressed=== expetedAlphabate){
+    if (playerPressed === expetedAlphabate) {
         console.log('You gat a point');
-      
-
- const currentScore = getTextGetElementVlaueId('current-score')
- const updatescore = currentScore + 1 ;
- setTextGetElementVlaueId('current-score',updatescore);
 
 
+        const currentScore = getTextGetElementVlaueId('current-score')
+        const updatescore = currentScore + 1;
+        setTextGetElementVlaueId('current-score', updatescore);
 
 
-//---------------------------------------------------
 
-       /*  // update score
-        // 1. get the current score
-        const currentScoreElement = document.getElementById('current-score')
-        const currentScoreText = currentScoreElement.innerText;
-        const currentScore = parseInt(currentScoreText);
-        console.log(currentScore);
-        
-        // 2. increase the score by 1
-        const newScore = currentScore + 1;
 
-        // show the update score
-        currentScoreElement.innerText = newScore; */
-//------------------------------------------------------
+        //---------------------------------------------------
+
+        /*  // update score
+         // 1. get the current score
+         const currentScoreElement = document.getElementById('current-score')
+         const currentScoreText = currentScoreElement.innerText;
+         const currentScore = parseInt(currentScoreText);
+         console.log(currentScore);
+         
+         // 2. increase the score by 1
+         const newScore = currentScore + 1;
+ 
+         // show the update score
+         currentScoreElement.innerText = newScore; */
+        //------------------------------------------------------
         removeBackGroundColor(expetedAlphabate);
         continueGame();
     }
-    else{
+    else {
         console.log('you mised .  loss a life')
 
         const currentLife = getTextGetElementVlaueId('current-life');
-        const updateLife = currentLife -1;
-        setTextGetElementVlaueId('current-life',updateLife);
+        const updateLife = currentLife - 1;
+        setTextGetElementVlaueId('current-life', updateLife);
 
-        if(updateLife === 0){
+        if (updateLife === 0) {
             gameOver();
             console.log('game over');
         }
@@ -72,18 +73,18 @@
 
         //---------------------------------------------
 
-       /*  // step-1: get the life count
-        const currentLifeElement = document.getElementById('current-life')
-        const currentLifeText = currentLifeElement.innerText;
-        const currentLife = parseInt(currentLifeText);
-
-        // step-2: display the upload life count
-        const newLife = currentLife - 1;
-
-        // step-3 : display the life count
-        currentLifeElement.innerText =newLife; */
+        /*  // step-1: get the life count
+         const currentLifeElement = document.getElementById('current-life')
+         const currentLifeText = currentLifeElement.innerText;
+         const currentLife = parseInt(currentLifeText);
+ 
+         // step-2: display the upload life count
+         const newLife = currentLife - 1;
+ 
+         // step-3 : display the life count
+         currentLifeElement.innerText =newLife; */
     }
- }
+}
 
 
 // capture keyboard press
@@ -113,22 +114,23 @@ function play() {
     hideElemnetId('home-scren');
     hideElemnetId('final-score')
     addElement('play-ground');
+
     // reset the score and life
-    setTextGetElementVlaueId('current-life',5)
-    setTextGetElementVlaueId('current-score',0)
+    setTextGetElementVlaueId('current-life', 5)
+    setTextGetElementVlaueId('current-score', 0)
 
     continueGame();
 }
 
 
-function gameOver(){
+function gameOver() {
     hideElemnetId('play-ground');
     addElement('final-score');
-    
+
     // update final score
     // 1. get the final score
-    const lastscore = getTextGetElementVlaueId ('current-score');
-    setTextGetElementVlaueId ('last-score',lastscore);
+    const lastscore = getTextGetElementVlaueId('current-score');
+    setTextGetElementVlaueId('last-score', lastscore);
 
     // clear the last selected alphabate highlight
     const currentAlphabatee = getTextElementId('current-alphabate')
